028d08d617ef84c1c4c4e0b0cba62385ab5d9de7
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-07-29 15:01:41 +0100

    3. List all the tables created

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,4 @@
--- This script will list all tables in the specified database
-SELECT table_name 
-FROM information_schema.tables 
-WHERE table_schema = 'alx_book_store';
+USE alx_book_store;
+
+SHOW TABLES;
 
